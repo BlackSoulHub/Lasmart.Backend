fb1a8a74287c07191c6c9967ea67635278bc8445
BlackSoulHub <mr.asaglaev@mail.ru>
2023-06-01 17:51:08 +0300

    Удалён лишний using

--- Lasmart.WebApi/Controllers/DotController.cs
+++ Lasmart.WebApi/Controllers/DotController.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Lasmart.Services.Interfaces;
 using Lastmart.Domain.Exceptions;
 using Lastmart.Domain.Requests.Dot;
